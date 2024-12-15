d9a0306be02d869bf41a6fb498c13392a93292da
Yap <yap@gmail.com>
2024-12-15 23:45:13 +0800

    webserv

---
+++ inc/ServerConfig.hpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerConfig.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyap <jyap@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 22:51:47 by jyap              #+#    #+#             */
+/*   Updated: 2024/12/15 23:13:46 by jyap             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVERCONFIG_HPP
+#define SERVERCONFIG_HPP
+
+#include "Webserv.hpp"
+
+class Location;
+
+class ServerConfig
+{
+	private:
+		uint16_t						_port;
+		in_addr_t						_host;
+		std::string						_server_name;
+		std::string						_root;
+		unsigned long					_client_max_body_size;
+		std::string						_index;
+		bool							_autoindex;
+		std::map<short, std::string>	_error_pages;
+		std::vector<Location> 			_locations;
+		struct sockaddr_in 				_server_address;
+		int								_listen_fd;
+
+	public:
+		ServerConfig();
+		~ServerConfig();
+		ServerConfig(const ServerConfig &other);
+		ServerConfig &operator=(const ServerConfig & rhs);
+
+		void initErrorPages(void);
+		void setServerName(std::string server_name);
+		void setHost(std::string parameter);
+		void setRoot(std::string root);
+		void setFd(int);
+		void setPort(std::string parameter);
+		void setClientMaxBodySize(std::string parameter);
+		void setErrorPages(std::vector<std::string> &parameter);
+		void setIndex(std::string index);
+		void setLocation(std::string nameLocation, std::vector<std::string> parameter);
+		void setAutoindex(std::string autoindex);
+		bool isValidHost(std::string host) const;
+		bool isValidErrorPages();
+		int isValidLocation(Location &location) const;
+		const std::string &getServerName();
+		const uint16_t &getPort();
+		const in_addr_t &getHost();
+		const size_t &getClientMaxBodySize();
+		const std::vector<Location> &getLocations();
+		const std::string &getRoot();
+		const std::map<short, std::string> &getErrorPages();
+		const std::string &getIndex();
+		const bool &getAutoindex();
+		const std::string &getPathErrorPage(short key);
+		const std::vector<Location>::iterator getLocationKey(std::string key);
+		static void checkToken(std::string &parameter);
+		bool checkLocaitons() const;
+
+		class ErrorException : public std::exception
+		{
+			private:
+				std::string _message;
+			public:
+				ErrorException(std::string message) throw()
+				{
+					_message = "SERVER CONFIG ERROR: " + message;
+				}
+				virtual const char* what() const throw()
+				{
+					return (_message.c_str());
+				}
+				virtual ~ErrorException() throw() {}
+		};
+
+		void	setupServer();
+		int		getFd();
+};
+
+#endif
