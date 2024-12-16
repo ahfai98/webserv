65d94a2ba1a69b8406754cabb33b2a17633c13a5
Yap <yap@gmail.com>
2024-12-16 11:42:22 +0800

    fix

--- inc/Mime.hpp
+++ inc/Mime.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Mime.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyap <jyap@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 11:19:09 by jyap              #+#    #+#             */
+/*   Updated: 2024/12/16 11:31:15 by jyap             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MIME_HPP
 #define MIME_HPP
 
@@ -5,15 +17,12 @@
 
 class Mime
 {
-    public:
-        Mime();
-        std::string getMimeType(std::string extension);
-    private:
-        std::map<std::string, std::string> _mime_types;
-        
+	public:
+		Mime();
+		static std::string toLower(const std::string &str);
+		std::string getMimeType(std::string extension);
+	private:
+		std::map<std::string, std::string> _mime_types;
 };
 
-
-
-
-#endif// MIME_HPP
+#endif
